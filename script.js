893ae448e094138a0e4043bc69b8d1869fe5af9a
Hajni83 <maro.hajnalka@gmail.com>
2018-07-21 22:40:25 +0200

    fine2

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ function triangle(x,y,a) {
     context.lineTo(x-a/2,y);
     context.lineTo(x, y-(Math.sqrt(Math.pow(a,2)-Math.pow(a/2,2))));
     context.lineTo(x+a/2, y);
+   
 }
 
-triangle(230,260,50);
-triangle(270,250,50);
-triangle(200,150,150);
-context.stroke();
+    triangle(230,210,50);
+    triangle(270,160,50);
+    triangle(200,200,150);
+    context.fillStyle="orange";
+    context.fill();
+    context.strokeStyle='gray';
+    context.stroke();
 });
